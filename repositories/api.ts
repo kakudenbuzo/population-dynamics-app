2bf455bb0f4a49e022817a5e775ff369a8830bbe
kakudenbuzo <kakudenbuzou@gmail.com>
2023-06-20 22:43:25 +0200

    style: セミコロン追加

--- repositories/api.ts
+++ repositories/api.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseURL = process.env.HOST_NAME + '/server-api'
+const baseURL = process.env.HOST_NAME + '/server-api';
 
 const api = axios.create({
   baseURL,
-})
+});
 
-export default api
+export default api;

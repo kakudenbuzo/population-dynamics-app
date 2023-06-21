25d3663414b72d3d77159c9b54f54c64aee2d4de
kakudenbuzo <kakudenbuzou@gmail.com>
2023-06-21 16:08:40 +0200

    style: セミコロン追加

--- server/api.ts
+++ server/api.ts
@@ -1,27 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseURL = 'https://opendata.resas-portal.go.jp/api/v1'
+const baseURL = 'https://opendata.resas-portal.go.jp/api/v1';
 
 const api = axios.create({
   baseURL,
-})
+});
 
-api.defaults.headers.common['X-API-KEY'] = process.env.API_KEY
+api.defaults.headers.common['X-API-KEY'] = process.env.API_KEY;
 
 export default async function (req: any, res: any, _next: any) {
   try {
     // APIエンドポイントのパスを取得
-    const path = req.originalUrl
-    const replaced = path.replace('/server-api', '')
+    const path = req.originalUrl;
+    const replaced = path.replace('/server-api', '');
 
     // クライアントからのリクエストをサーバーサイドで実行
-    const response = await api.get(replaced)
+    const response = await api.get(replaced);
 
     // レスポンスをクライアントに返す
-    res.statusCode = response.status
-    res.end(JSON.stringify(response.data))
+    res.statusCode = response.status;
+    res.end(JSON.stringify(response.data));
   } catch (error) {
-    res.statusCode = 500
-    res.end('Server Error')
+    res.statusCode = 500;
+    res.end('Server Error');
   }
 }
